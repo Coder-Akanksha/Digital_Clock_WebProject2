fffbdd0bf176b8e2f16bb3f3de11fc675187ce22
Coder-Akanksha <shivi.aka1810@gmail.com>
2024-09-16 00:05:23 +0530

    add Javascript to the clock

---
+++ script.js
@@ -0,0 +1,26 @@
+const time = document.getElementById('time');
+const timeformat = document.getElementById('timeformate');
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    setInterval(showTime, 1000);
+})
+
+const showTime = () =>{
+    let date = new Date();
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hr = hr<10 ? `0${hr}` : hr;
+    min = min<10 ? `0${min}` :min;
+   sec = sec<10 ? `0${sec}` : sec;
+    
+    
+
+    time.innerHTML =` ${hr} : ${min} : ${sec}`;
+
+    timeformat.innerHTML = time>12 ? "PM" : "AM";
+
+    //console.log("hours" + hr + "min" + min + "sec" + sec);
+}
